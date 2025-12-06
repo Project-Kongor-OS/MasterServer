f4d2d3c35ba716776f49a1bde3c61b2a82b0582e
Mark Klara <mrhappyasthma@gmail.com>
2025-12-06 11:09:39 -0800

    Rename SKELETON-KING to Server and fix up dependencies.

---
+++ Server/ClientRequestHandlerRegistry.cs
@@ -0,0 +1,21 @@
+﻿using KONGOR.Shared.Handlers.Client;
+using ProjectKongor.Protocol.Handlers;
+using ProjectKongor.Protocol.Services;
+using ProjectKongor.Protocol.Registries;
+
+namespace SKELETON_KING;
+
+public class ClientRequestHandlerRegistry : IClientRequestHandlerRegistry
+{
+	public IReadOnlyDictionary<string, IClientRequestHandler> Handlers { get; }
+
+	public ClientRequestHandlerRegistry(IAccountService accountService, IPlayerStatsService statsService)
+	{
+		Handlers = new Dictionary<string, IClientRequestHandler>()
+		{
+            // Alphabetized by protocol key
+            {"nick2id", new Nick2IdHandler(accountService)},
+			{"show_simple_stats" , new ShowSimpleStatsHandler(statsService)}
+		};
+	}
+}
